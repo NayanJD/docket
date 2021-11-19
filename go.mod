6ffd75d3035a6f18e84a081d831c54c9ba5bcacf
Nayan Jyoti Das <dastms@gmail.com>
2021-11-19 20:21:01 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module nayanjd/docket
+
+go 1.14
+
+require (
+	github.com/dn365/gin-zerolog v0.0.0-20171227063204-b43714b00db1
+	github.com/gin-gonic/gin v1.7.4
+	github.com/rs/zerolog v1.26.0
+)
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	ginzerolog "github.com/dn365/gin-zerolog"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	r := gin.New()
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	r.Use(ginzerolog.Logger("gin"))
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to docket",
+		})
+	})
+	log.Printf("Server stopped, err: %v", r.Run(":4000"))
+}
