2fb84a16a3caf4c9637e6627d2e184acf4590dd5
Nayan Das <dastms@gmail.com>
2021-11-22 19:51:50 +0530

    Pick sql creds from env and add .env and others in gitignore

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/rs/zerolog/log"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,15 +12,23 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// username := os.Getenv("")
-	// mysqlDsn := fmt.Sprintf("")
-	database, err := gorm.Open(mysql.Open("go:password@tcp(127.0.0.1:3306)/docket_local"), &gorm.Config{})
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	database_name := os.Getenv("DB_DATABASE")
+
+	mysqlDsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", username, password, host, port, database_name)
+
+	database, err := gorm.Open(mysql.Open(mysqlDsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to mysql")
 	}
 
-	log.Info().Msg("Successfully connected to postgres")
+	log.Info().Msg("Successfully connected to mysql")
+	log.Info().Msg("Test restart")
 
 	err = database.AutoMigrate(&User{})
 	
